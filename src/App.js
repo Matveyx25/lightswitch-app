e9724cfbbaccad99733b9a01a0ff24dd2689d882
Matveyx25 <matveyx25@gmail.com>
2022-01-31 00:35:22 +0400

    initialize cursor

--- src/App.js
+++ src/App.js
@@ -5,12 +5,20 @@ import { HomePage } from './pages/HomePage/HomePage';
 import { Footer } from './Components/Footer/Footer';
 import { ProjectsPage } from './pages/ProjectsPage/ProjectsPage';
 import { ContactPage } from './pages/ContactPage/ContactPage';
+import AnimatedCursor from 'react-animated-cursor';
 
 function App() {
   
 
   return (
     <BrowserRouter>
+      <AnimatedCursor 
+      innerSize={10}
+      outerSize={60}
+      color='225, 225, 225'
+      outerAlpha={0.2}
+      innerScale={2}
+      outerScale={1.2}/>
       <Header/>
         <Routes>
           <Route path="/portfolio" element={<ProjectsPage/>} />
